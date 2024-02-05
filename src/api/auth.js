75105915c21ada9f970fc5fbb080cd3e5e916587
Willian Rafael <willian.rafael18@gmail.com>
2024-02-05 14:18:17 -0300

    feat: adicionado geração de cookies

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,22 +1,20 @@
 const facade = require('../facade/validateAcl')
+const moment = require('moment')
 
 const validate = async (req, res, next) => {
-  // if (req.cookies.cookieID) {
-    
-  //   const url = req.originalUrl.split('/')[1].split('?')[0];
-
-  //   result = await facade.validateAcl(req.cookies.cookieID, `/${url}`)
-
-  //   if (result === true) {
-  //     return next()
-  //   } else {
-  //     return res.sendStatus(401)
-  //   }
-  // } else {
-  //   return res.sendStatus(401)
-  // }
   if (req.cookies.cookieID) {
-    return next()
+    // const url = req.originalUrl.split('/')[1].split('?')[0]
+
+    const expiration = moment().add(1, "hours").toDate();
+    res.cookie("cookieID", req.cookies.cookieID, { expires: expiration, httpOnly: true });
+
+    // result = await facade.validateAcl(req.cookies.cookieID, `/${url}`)
+
+    // if (result === true) {
+      return next()
+    // } else {
+    //   return res.sendStatus(401)
+    // }
   } else {
     return res.sendStatus(401)
   }
